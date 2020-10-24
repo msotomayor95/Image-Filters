e146c6cb2e5a2244aff363419c2296d91ade333b
Marco Sotomayor <msotomayor@dc.uba.ar>
2020-10-24 16:07:01 -0300

    Seguimos con Reforzar Brillo

--- src/filters/ReforzarBrillo_asm.asm
+++ src/filters/ReforzarBrillo_asm.asm
@@ -22,25 +22,33 @@ ReforzarBrillo_asm:
     ; [rsp+40] = brilloInf
 
     push rbp
-    mov rbp, rsp        ; StackFrame
+    mov rbp, rsp    ; StackFrame
 
-    movaps xmm1, [extiende_y_copia_green_alto]
-    movaps xmm0, [extiende_y_copia_green_bajo]
+    movaps xmm15, [extiende_y_copia_green_alto]
+    movaps xmm14, [extiende_y_copia_green_bajo]
+
+    pxor xmm13, xmm13
+    movd xmm13, [rbp + 16]      ; Muevo umbralSup a la parte baja xmm13
+    pshufd xmm13, xmm13, 0x00   ; [ umbralSup | umbralSup | umbralSup | umbralSup ]
+
+    pxor xmm12, xmm12
+    movd xmm12, [rbp + 24]      ; Muevo umbralInf a la parte baja de xmm12
+    pshufd xmm12, xmm12, 0x00   ; [ umbralInf | umbralInf | umbralInf | umbralInf ]
+
 
     movups xmm2, [rdi]  ; xmm1 = [ a_3 | r_3 | g_3 | b_3 | ... ]
-    movaps xmm3, xmm2
-    movaps xmm4, xmm2
+    movaps xmm3, xmm1
     
-    xor xmm5, xmm5
+    xor xmm4, xmm4
 
-    pshufb xmm3, xmm0   ; xmm3 = [ pixel 1 | pixel 0 ] y con el formato [ G | R | G | B ] para facilitar la suma
-    pshufb xmm4, xmm1   ; xmm4 = [ pixel 3 | pixel 2 ] con el mismo formato
+    pshufb xmm2, xmm14  ; xmm3 = [ pixel 1 | pixel 0 ] y con el formato [ G | R | G | B ] para facilitar la suma
+    pshufb xmm3, xmm15  ; xmm4 = [ pixel 3 | pixel 2 ] con el mismo formato
     
-    phaddw xmm3, xmm5   ; xmm3 = [ 0 | 0 | 0 | 0 | G + R (pixel1) | G + B (pixel1) | G + R (pixel0) | G + B (pixel0) ]
-    phaddw xmm4, xmm5   ; xmm4 = [ 0 | 0 | 0 | 0 | G + R (pixel3) | G + B (pixel2) | G + R (pixel3) | G + B (pixel2) ]
+    phaddw xmm3, xmm4   ; xmm3 = [ 0 | 0 | 0 | 0 | G + R (pixel1) | G + B (pixel1) | G + R (pixel0) | G + B (pixel0) ]
+    phaddw xmm4, xmm4   ; xmm4 = [ 0 | 0 | 0 | 0 | G + R (pixel3) | G + B (pixel2) | G + R (pixel3) | G + B (pixel2) ]
 
-    phaddw xmm3, xmm5   ; xmm3 = [ 0 | 0 | 0 | 0 | R + 2G + B (pixel1) | R + 2G + B (pixel0) | R + 2G + B (pixel1) | R + 2G + B (pixel0) ]
-    phaddw xmm4, xmm5   ; xmm4 = [ 0 | 0 | 0 | 0 | R + 2G + B (pixel3) | R + 2G + B (pixel2) | R + 2G + B (pixel3) | R + 2G + B (pixel2) ]
+    phaddw xmm3, xmm4   ; xmm3 = [ 0 | 0 | 0 | 0 | R + 2G + B (pixel1) | R + 2G + B (pixel0) | R + 2G + B (pixel1) | R + 2G + B (pixel0) ]
+    phaddw xmm4, xmm4   ; xmm4 = [ 0 | 0 | 0 | 0 | R + 2G + B (pixel3) | R + 2G + B (pixel2) | R + 2G + B (pixel3) | R + 2G + B (pixel2) ]
     
     psrldq xmm3, 4
     psrldq xmm4, 4
@@ -48,8 +56,24 @@ ReforzarBrillo_asm:
     pslldq xmm4, 4
     por xmm3, xmm4      ; xmm3 = [ 0 | 0 | 0 | 0 | R + 2G + B (pixel3) | R + 2G + B (pixel2) | R + 2G + B (pixel1) | R + 2G + B (pixel0) ]
 
-    psrlw xmm4, 2       ; xmm3 = [ 0 | 0 | 0 | 0 | (R + 2G + B)/4 (pixel3) | (R + 2G + B)/4 (pixel2) | (R + 2G + B)/4 (pixel1) | (R + 2G + B)/4 (pixel0) ]
-    movdqa xmm3, xmm4   ; copias para facilitar
-    movdqa xmm2, xmm4   ; la cantidad 
+    psrlw xmm3, 2       ; xmm3 = [ 0 | 0 | 0 | 0 | (R + 2G + B)/4 (pixel3) | (R + 2G + B)/4 (pixel2) | (R + 2G + B)/4 (pixel1) | (R + 2G + B)/4 (pixel0) ]
+    
+    pmovzxwd xmm3, xmm3 ; xmm3 = [ (R + 2G + B)/4 (pixel3) | (R + 2G + B)/4 (pixel2) | (R + 2G + B)/4 (pixel1) | (R + 2G + B)/4 (pixel0) ]
+    movdqa xmm4, xmm3   ; copia. Ahora a la cuenta la llamare b
+
+    pcmpgtd xmm3, xmm13 ; xmm3 = [ b > umbralSup (pixel3) | b > umbralSup (pixel2) | b > umbralSup (pixel1) | b > umbralSup (pixel0) ]
+    movdqa xmm5, xmm3   ; copia
+
+    pandn xmm5, xmm4    ; tomo los b que no cumpliero con la condicion
+    movdqa xmm6, xmm12
+    pcmpgtd xmm6, xmm5  ; xmm6 = [ b > umbralSup (pixel3) | b > umbralSup (pixel2) | b > umbralSup (pixel1) | b > umbralSup (pixel0) ]
+
+
+
+    
+
+    ; movdqa xmm5, xmm12
+    ; pcmpgtd xmm5, xmm4  ; xmm5 = [ umbralInf > (R + 2G + B)/4 (pixel3) | umbralInf > (R + 2G + B)/4 (pixel2) | umbralInf > (R + 2G + B)/4 (pixel1) | umbralInf > (R + 2G + B)/4 (pixel0) ]
+
     
 ret
