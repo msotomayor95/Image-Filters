9f2e4db4741de61a9b86f0e2ef84b1c5cbb2d18c
wtejera <wtejera@dc.uba.ar>
2020-10-25 21:07:35 -0300

    modif a imgfantasma_c para debuguear

--- src/filters/ImagenFantasma_c.c
+++ src/filters/ImagenFantasma_c.c
@@ -28,12 +28,16 @@ void ImagenFantasma_c(
             int ii = i/2 + offsety;
             int jj = j/2 + offsetx;
 
+
             float rrr = (float)src_matrix[ii][jj].r;
             float ggg = (float)src_matrix[ii][jj].g;
             float bbb = (float)src_matrix[ii][jj].b;
 
             float b = (rrr + 2 * ggg + bbb)/4;
 
+            float tempR = SAT( rr * 0.9 + b/2 );
+            float tempG = SAT( gg * 0.9 + b/2 );
+            float tempB = SAT( bb * 0.9 + b/2 );
             dst_matrix[i][j].r = SAT( rr * 0.9 + b/2 );
             dst_matrix[i][j].g = SAT( gg * 0.9 + b/2 );
             dst_matrix[i][j].b = SAT( bb * 0.9 + b/2 );
