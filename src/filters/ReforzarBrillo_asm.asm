807e1f4f962fe160ad2e20fc479b74c23b850c24
Marco Sotomayor <msotomayor@dc.uba.ar>
2020-10-26 21:58:10 -0300

    Filtro de Brillo casi terminado, falta debuggear

--- src/filters/ReforzarBrillo_asm.asm
+++ src/filters/ReforzarBrillo_asm.asm
@@ -12,10 +12,10 @@ section .text
 ReforzarBrillo_asm:
     ; rdi = *src
     ; rsi = *dst
-    ; rdx = width 
-    ; rcx = height
-    ; r8 = src_row_size
-    ; r9 = dst_row_size
+    ; edx = width 
+    ; ecx = height
+    ; r8d = src_row_size
+    ; r9d = dst_row_size
     ; [rbp+16] = umbralSup
     ; [rbp+24] = umbralInf
     ; [rbp+32] = brilloSup
@@ -40,22 +40,78 @@ ReforzarBrillo_asm:
     movd xmm11, [rbp + 40]      ; Muevo umbralInf a la parte baja de xmm10
     pshufd xmm11, xmm11, 0x00   ; [ brilloInf | brilloInf | brilloInf | brilloInf ]
     
-    packusdw xmm14, xmm9        ; [ 0 | 0 | 0 | 0 | umbralSup | umbralSup | umbralSup | umbralSup ]
-    packusdw xmm13, xmm9        ; [ 0 | 0 | 0 | 0 | umbralInf | umbralInf | umbralInf | umbralInf ]
-    packusdw xmm12, xmm9        ; [ 0 | 0 | 0 | 0 | brilloSup | brilloSup | brilloSup | brilloSup ]
-    packusdw xmm11, xmm10       ; [ 0 | 0 | 0 | 0 | brilloInf | brilloInf | brilloInf | brilloInf ]
+    packusdw xmm14, xmm9        
+    psrldq xmm14, 4             ; [ 0 | 0 | 0 | 0 | 0 | 0 | umbralSup | umbralSup ]
 
-    movdqu xmm0, [rdi]  ; xmm1 = [ a_3 | r_3 | g_3 | b_3 | ... ]
-    movdqa xmm1, xmm0
-    movdqa xmm2, xmm0  
+    packusdw xmm13, xmm9        
+    psrldq xmm13, 4             ; [ 0 | 0 | 0 | 0 | 0 | 0 | umbralInf | umbralInf ]
+
+    packusdw xmm12, xmm9        
+    psrldq xmm12, 4             ; [ 0 | 0 | 0 | 0 | 0 | 0 | brilloSup | brilloSup ]
     
-    pshufb xmm0, xmm14  ; xmm3 = [ pixel 1 | pixel 0 ] y con el formato [ G | R | G | B ] para facilitar la suma
+    packusdw xmm11, xmm9        
+    psrldq xmm11, 4             ; [ 0 | 0 | 0 | 0 | 0 | 0 | brilloSup | brilloSup ]
+
+    xor rdx, rdx
+    mov eax, r8d 
+    mul ecx
+    shl rdx, 32
+    or rax, rdx
+    add rax, rsi
     
-    phaddw xmm1, xmm8   ; xmm3 = [ 0 | 0 | 0 | 0 | G + R (pixel1) | G + B (pixel1) | G + R (pixel0) | G + B (pixel0) ]
+    .ciclo_brillos:
+        cmp rax, rsi
+        je .fin
 
-    phaddw xmm1, xmm8   ; xmm3 = [ 0 | 0 | 0 | 0 | 0 | 0 | R + 2G + B (pixel1) | R + 2G + B (pixel0) ]
+        movdqu xmm0, [rdi]  ; xmm0 = [ a_3 | r_3 | g_3 | b_3 | ... ]
+        movdqa xmm1, xmm0
+        
+        pshufb xmm0, xmm14  ; xmm0 = [ pixel 1 | pixel 0 ] y con el formato [ G | R | G | B ] para facilitar la suma
+        
+        phaddw xmm0, xmm9   ; xmm0 = [ 0 | 0 | 0 | 0 | G + R (pixel1) | G + B (pixel1) | G + R (pixel0) | G + B (pixel0) ]
 
-    psrlw xmm1, 2       ; xmm3 = [ 0 | 0 | 0 | 0 | 0 | 0 | (R + 2G + B)/4 (pixel1) | (R + 2G + B)/4 (pixel0) ]
+        phaddw xmm0, xmm9   ; xmm0 = [ 0 | 0 | 0 | 0 | 0 | 0 | R + 2G + B (pixel1) | R + 2G + B (pixel0) ]
 
-    
+        psrlw xmm0, 2       ; xmm0 = [ 0 | 0 | 0 | 0 | 0 | 0 | (R + 2G + B)/4 (b de pixel1) | (R + 2G + B)/4 (b de pixel0) ]
+        movdqa xmm3, xmm0   ; copia
+
+        pcmpgtw xmm0, xmm14 ; xmm0 = [ 0 | 0 | 0 | 0 | 0 | 0 | b pixel1 > umbralSup | b pixel 0 > umbralSup ]
+        
+        movdqa xmm4, xmm0
+        pandn xmm4, xmm1    ; me quedo con los b que no cumpleron la condicion
+
+        movdqa xmm5, xmm13  ; copia umbralInf
+        pcmpgtw xmm5, xmm4  ; xmm5 = [ 0 | 0 | 0 | 0 | 0 | 0 | umbralInf > b pixel1 | umbralInf > b pixel0 ]
+
+        pand xmm4, xmm12    ; xmm4 = [ 0 | 0 | 0 | 0 | 0 | 0 | brillSup (si b de p1 > uS) | brilloSup si b de p0 > uS ]
+        pand xmm5, xmm11    ; xmm5 = [ 0 | 0 | 0 | 0 | 0 | 0 | brillInf (si b de p1 < uI) | brilloInf si b de p0 < uI ]
+
+        punpcklbw xmm1, xmm9        ; xmm1 = [ A1 | R1 | G1 | B1 | A0 | R0 | G0 | B0 ]
+
+        ; /////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+        pshuflw xmm6, xmm4, 0xC0    ; xmm6 = [ kk | kk | kk | kk | 00 | bS | bS | bS ]
+        pslldq xmm4, 4
+        pshufhw xmm6, xmm4, 0xD5    ; xmm6 = [ 00 | bS | bS | bS | 00 | bS | bS | bS ]
+
+        paddusw xmm1, xmm6
+        
+        ; /////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+        pshuflw xmm6, xmm5, 0xC0    ; xmm6 = [ kk | kk | kk | kk | 00 | bI | bI | bI ]
+        pslldq xmm5, 4
+        pshufhw xmm6, xmm5, 0xD5     ; xmm6 = [ 00 | bI | bI | bI | 00 | bI | bI | bI ]
+
+        psubusw xmm1, xmm5
+
+        packuswb xmm1, xmm9
+
+        movq [rsi], xmm1
+
+        lea rsi, [rsi + 8]
+        lea rdi, [rdi + 8]
+        jmp .ciclo_brillos
+
+    .fin:
+    pop rbp
 ret
